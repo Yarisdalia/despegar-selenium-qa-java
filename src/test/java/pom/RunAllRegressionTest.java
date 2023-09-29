d6c2266b7020dbe31fdea013e464566ed1c89fa5
Yarisdalia <yarisdaliahinojosapadilla2001@gmail.com>
2023-09-28 22:55:29 -0300

    test

--- src/test/java/pom/RunAllRegressionTest.java
+++ src/test/java/pom/RunAllRegressionTest.java
@@ -5,11 +5,10 @@ import org.junit.runners.Suite;
 import pom.alojamiento.AlojamientoSinFechaTest;
 import pom.alojamiento.AlojamientoTest;
 import pom.home.HomeTest;
-import pom.login.LoginTest;
 import pom.ofertas.OfertasTest;
 import pom.vuelos.VuelosTest;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({LoginTest.class, AlojamientoTest.class, AlojamientoSinFechaTest.class, VuelosTest.class, OfertasTest.class, HomeTest.class})
+@Suite.SuiteClasses({AlojamientoTest.class, AlojamientoSinFechaTest.class, VuelosTest.class, OfertasTest.class, HomeTest.class})
 public class RunAllRegressionTest {
 }
