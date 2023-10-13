f56dae232cdd1743175db128bd476459efe6df51
Yarisdalia <yarisdaliahinojosapadilla2001@gmail.com>
2023-10-13 00:09:31 -0300

    refactor

--- src/test/java/RunAllRegressionTest.java
+++ src/test/java/RunAllRegressionTest.java
@@ -2,10 +2,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-<<<<<<< HEAD
 @Suite.SuiteClasses({AlojamientoTest.class, AlojamientoSinFechaTest.class, VuelosTest.class, OfertasTest.class, HomeTest.class})
-=======
-@Suite.SuiteClasses({LandingTest.class})
->>>>>>> 2bee3d4d78ebf4342919c35e84946fd2fc316717
 public class RunAllRegressionTest {
 }
